25ce23025209ee8e2be95e526fb92207aa7dbaea
Rekicho <up201604145@fe.up.pt>
2018-05-07 14:49:46 +0100

    Added wheel texture

--- WebCGF.tp6/tp6/MyWheel.js
+++ WebCGF.tp6/tp6/MyWheel.js
@@ -13,22 +13,29 @@ class MyWheel extends CGFobject
 		this.inside = new MyCylinder(scene,20,20,false);
 		this.fronttire = new MyRing(scene,20,0.75,1,true);
 		this.backtire = new MyRing(scene,20,0.75,1,false);
+
+		this.wheelAppearence = new CGFappearance(this.scene);
+		this.wheelAppearence.loadTexture("../resources/images/wheel.png");
 	};
 
 	display()
 	{
-		this.outside.display();
-
 		this.scene.pushMatrix();
-			this.scene.scale(0.75,0.75,1);
-			this.inside.display();
-		this.scene.popMatrix();
+			this.scene.translate(0,0,-0.5);
+			this.scene.pushMatrix();
+				this.scene.scale(0.75,0.75,1);
+				this.inside.display();
+			this.scene.popMatrix();
 
-		this.scene.pushMatrix();
-			this.scene.translate(0,0,1);
-			this.fronttire.display();
-		this.scene.popMatrix();
+			this.scene.pushMatrix();
+				this.scene.translate(0,0,1);
+				this.fronttire.display();
+			this.scene.popMatrix();
 
-		this.backtire.display();
+			this.backtire.display();
+
+			this.wheelAppearence.apply();
+			this.outside.display();
+		this.scene.popMatrix();
 	};
 };
