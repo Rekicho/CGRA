51960e7bd39e8353f03e48a771d17b8df1416662
Rekicho <up201604145@fe.up.pt>
2018-05-26 23:40:40 +0100

    Tempestade de areia

--- WebCGF.tp6/tp6/LightingScene.js
+++ WebCGF.tp6/tp6/LightingScene.js
@@ -63,8 +63,26 @@ class LightingScene extends CGFscene
 
 		//Scene elements
 
+		//Carro
 		this.car = new MyVehicle(this, this.chassistexture, this.camo);
-		this.terrain = new MyTerrain(this,8,0,5,0,5);
+		
+		//Terreno
+
+		this.altimetry= [[ 4.3 , 4.5 , 2.8 , 2.2 , 4.3 , 0.5 , 4.9 , 0.6 , 2.7 , 0.6 , 4.9 , 9.7 , 1.1 ],
+						 [ 9.0 , 4.6 , 0.7 , 0.2 , 6.3 , 0.3 , 7.6 , 7.1 , 2.3 , 2.5 , 1.2 , 9.1 , 4.2 ],
+						 [ 3.2 , 3.5 , 1.3 , 8.5 , 7.3 , 5.6 , 3.1 , 5.4 , 3.1 , 2.6 , 5.9 , 8.0 , 3.2 ],
+						 [ 3.9 , 3.8 , 3.4 , 3.6 , 0.1 , 2.4 , 3.5 , 6.0 , 2.6 , 5.0 , 1.5 , 5.4 , 7.3 ],
+						 [ 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ],
+						 [ 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ],
+						 [ 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ],
+						 [ 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ],
+						 [ 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ],
+						 [ 7.8 , 1.7 , 8.6 , 9.7 , 0.5 , 9.9 , 0.5 , 8.9 , 9.8 , 1.4 , 5.2 , 1.5 , 7.8 ],
+						 [ 2.5 , 8.1 , 9.2 , 4.6 , 8.2 , 4.2 , 1.8 , 4.3 , 1.3 , 0.4 , 4.2 , 0.9 , 6.8 ],
+						 [ 3.4 , 5.4 , 4.8 , 0.1 , 3.4 , 7.9 , 7.0 , 7.2 , 7.6 , 7.6 , 7.1 , 3.3 , 6.5 ],
+						 [ 2.2 , 4.7 , 6.9 , 8.9 , 7.7 , 9.4 , 7.0 , 7.0 , 4.1 , 5.2 , 6.4 , 1.1 , 9.6 ],
+						];
+		this.terrain = new MyTerrain(this,12,0,5,0,5,this.altimetry);
 
 		//Test Obejcts
 
@@ -200,8 +218,8 @@ class LightingScene extends CGFscene
 		{
 			//Cilindro
 			this.pushMatrix();
-				this.translate(-24,1,24);
-				this.rotate(Math.PI/2,0,1,0);
+				this.translate(-24,1,-7);
+				this.rotate(-Math.PI/10,0,1,0);
 				this.rotate(Math.PI/2,1,0,0);
 				this.testAppearance.apply();
 				this.testCylinderIn.display();
@@ -210,7 +228,8 @@ class LightingScene extends CGFscene
 
 			//Semiesfera
 			this.pushMatrix();
-				this.translate(-22,0,24);
+				this.translate(-24,0,-5);
+				this.rotate(-Math.PI/2,0,1,0);
 				this.rotate(-Math.PI/2,1,0,0);
 				this.testAppearance.apply();
 				this.testLamp.display();
@@ -218,7 +237,8 @@ class LightingScene extends CGFscene
 
 			//Trapézio
 			this.pushMatrix();
-				this.translate(-21,0,23.5);
+				this.translate(-23.5,0,-4);
+				this.rotate(-Math.PI/2,0,1,0);
 				this.scale(0.5,0.5,0.5);
 				this.testAppearance.apply();
 				this.testTrap.display();
@@ -226,7 +246,7 @@ class LightingScene extends CGFscene
 
 			//Cubo
 			this.pushMatrix();
-				this.translate(-18,0.5,24.5);
+				this.translate(-24.5,0.5,-1);
 				this.testAppearance.apply();
 				this.testCube.display();
 			this.popMatrix();
--- WebCGF.tp6/tp6/MyTerrain.js
+++ WebCGF.tp6/tp6/MyTerrain.js
@@ -6,24 +6,94 @@
 
 class MyTerrain extends Plane
 {
-	constructor(scene,nrDivs,minS, maxS, minT, maxT) 
+
+	constructor(scene,nrDivs,minS, maxS, minT, maxT,altimetry) 
 	{
 		super(scene,nrDivs,minS, maxS, minT, maxT);
-		
+
+		if(altimetry == null || nrDivs + 1 != altimetry.length || nrDivs + 1 != altimetry[0].length )
+			this.defaultAltimetry(nrDivs);
+
+		else this.altimetry = altimetry;
+   		
 		this.texture = new CGFappearance(this.scene);
 		this.texture.loadTexture("../resources/images/sand.png");
 		this.texture.setAmbient(1,1,1,1);
+
+		this.buffers();
+	};
+
+	defaultAltimetry(nrDivs)
+	{
+		var alti = [];
+
+		for(var i = 0; i <= nrDivs ; i++)
+		{
+			alti.push([0]);
+			for(var j = 0; j <= nrDivs ; j++)
+				alti[i][j] = 0;
+		}	
+		this.altimetry = alti;
+	};
+
+	buffers()
+	{
+		this.vertices = [];
+		this.normals = [];
+		
+		this.texCoords = [];
+
+		var yCoord = 0.5;
+
+		for (var j = 0; j <= this.nrDivs; j++) 
+		{
+			var xCoord = -0.5;
+			for (var i = 0; i <= this.nrDivs; i++) 
+			{
+				this.vertices.push(xCoord, yCoord,this.altimetry[j][i]);
+				
+				this.normals.push(0,0,1);
+							
+				this.texCoords.push(this.minS + ((this.maxS - this.minS) * (i / this.nrDivs)));
+				this.texCoords.push(this.minT + ((this.maxT - this.minT) * (j / this.nrDivs)));
+
+				xCoord += this.patchLength;
+			}
+			yCoord -= this.patchLength;
+		}
+			
+		this.indices = [];
+		var ind=0;
+
+		for (var j = 0; j < this.nrDivs; j++) 
+		{
+			for (var i = 0; i <= this.nrDivs; i++) 
+			{
+				this.indices.push(ind);
+				this.indices.push(ind+this.nrDivs+1);
+
+				ind++;
+			}
+
+			if (j+1 < this.nrDivs)
+			{			
+				this.indices.push(ind+this.nrDivs);
+				this.indices.push(ind);
+			}
+		}
+		
+		this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
+
+		this.initGLBuffers();
 	};
 
 	display()
-	{		
+	{	
 		this.scene.pushMatrix();
-		this.scene.scale(50,1,50);
 		this.scene.rotate(-Math.PI/2,1,0,0);
+		this.scene.scale(50,50,1);
 		this.texture.apply();
 		this.drawElements(this.primitiveType);
 		this.scene.popMatrix();
 	};
-
-	
 };
